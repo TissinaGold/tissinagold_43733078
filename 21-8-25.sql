7bcdb8ebee4183aa8372548b68886f2b3b0b1a23
TISSINA GOLD TS <157792288+TissinaGold@users.noreply.github.com>
2025-08-22 15:00:14 +0530

    Add files via upload

---
+++ 21-8-25.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE StudentDB;
+USE StudentDB;
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Age INT,
+    Department VARCHAR(50),
+    Marks INT
+);
+INSERT INTO Students (Name, Age, Department, Marks)
+VALUES 
+('Tissina', 19, 'Data Science', 85),
+('Reshma', 20, 'AI & ML', 90),
+('Akshaya', 19, 'Cybersecurity', 78),
+('Kommi', 21, 'IoT', 82);
+SELECT * FROM Students;
+ALTER TABLE Students
+ADD Email VARCHAR(100);
+ALTER TABLE Students
+MODIFY Marks DECIMAL(5,2);
+ALTER TABLE Students
+CHANGE Age StudentAge INT;
+ALTER TABLE Students
+DROP COLUMN Email;
+SELECT * FROM Students;
